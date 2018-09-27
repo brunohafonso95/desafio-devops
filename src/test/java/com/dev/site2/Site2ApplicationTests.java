c81eb2d8db8721b6ef552e76d55c946ea8b2f9ad
brunohafonso95 <43271781+brunohafonso95@users.noreply.github.com>
2018-09-27 11:55:12 -0300

    Update Site2ApplicationTests.java

--- src/test/java/com/dev/site2/Site2ApplicationTests.java
+++ src/test/java/com/dev/site2/Site2ApplicationTests.java
@@ -14,44 +14,8 @@ import static org.junit.Assert.assertEquals;
 
 
 public class Site2ApplicationTests {
-    public static String driverpath = "C:/Users/bhenriquea/drivers/chromedriver/chromedriver.exe";
-
-    WebDriver driver = null;
-
-    @BeforeClass
-    public static void setupClass() {
-        WebDriverManager.firefoxdriver().proxy("proxylatam.indra.es:8080").setup();
-    }
-
-    @Before
-    public void abrirNavegador() {
-        System.out.println("abrindo navegador");
-        // setando caminho do driver
-        //System.setProperty("webdriver.chrome.driver", driverpath);
-        // iniciando webdriver do chrome
-        //FirefoxOptions options = new FirefoxOptions();
-        //options.addArguments("--headless");
-        //driver = new FirefoxDriver(options);
-        // maximizando o navegador
-        //driver.manage().window().maximize();
-        // acessando uma página no chrome
-        //driver.get("http://localhost:8090");
-    }
-
-    @After
-    public void encerrar() {
-        System.out.println("fechando navegador");
-        //driver.quit();
-    }
-
     @Test
 	public void preencherFormulario() {
-		//driver.findElement(By.id("input_name")).sendKeys("Teste");
-// 		driver.findElement(By.id("input_email")).sendKeys("teste@teste.com");
-// 		driver.findElement(By.id("input_assunto")).sendKeys("testando");
-// 		driver.findElement(By.id("input_mensagem")).sendKeys("testando 123");
-// 		driver.findElement(By.id("submit_button")).click();
-// 		String text = driver.findElement(By.id("input_mensagem")).getAttribute("value");
 		assertEquals("", "");
 	}
 }
